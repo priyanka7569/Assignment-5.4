e109b3736bcc43610fa2945f3e91540a66bdb18b
priyanka7569 <kota.priyanka@mphasis.com>
2023-11-09 17:53:01 +0530

    this is my 5.4 program

---
+++ Assignment 5.4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_5._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int score;
+            Console.WriteLine("Enter your score");
+            score = int.Parse(Console.ReadLine());
+            if (score >= 90)
+            {
+                Console.WriteLine("Grade: A");
+            }
+            else if (score >= 80 & score <= 89)
+            {
+                Console.WriteLine("Grade: B");
+            }
+            else if (score >= 70 & score <= 79)
+            {
+                Console.WriteLine("Grade: C");
+            }
+            else if (score >= 60 & score <= 69)
+            {
+                Console.WriteLine("Grade: D");
+            }
+            else if (score < 60)
+            {
+                Console.WriteLine("Grade: F");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input");
+            }
+            Console.ReadKey();
+        }
+    }
+}
